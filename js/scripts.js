eac451e6019c80e68b9fdcd34278d9e811f7519b
Jamison Cozart and Zakkrey Short <zakkrey@>
2020-01-22 15:06:57 -0800

    Add test_prime function to test each number of numArray, and display all prime numbers

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,32 @@ $(document).ready(function() {
     event.preventDefault();
     var input = $("#maxNum").val();
     var numArr = [];
+    var primeArr = [];
     for(var i = 2; i <= input; i += 1) {
       numArr.push(i);
     }
     console.log(numArr);
+    for(var i = 0; i < numArr.length; i++){
+      if (test_prime(numArr[i])) {
+        primeArr.push(numArr[i]);
+      }
+    }
+    $("#output").text(primeArr.join(", "));
   })
 })
+
+//returns true if num is prime or false if num is not
+function test_prime(num){
+  if (num===1){
+    return false;
+  } else if(num===2){
+    return true;
+  } else {
+    for(var x=2; x<num; x++){
+      if(num % x === 0){
+        return false;
+      }
+    }
+    return true;
+  }
+}
